a4906329336e9430e27a227242610ba01cb32b41
AVi Patel <avip1607@gmail.com>
2023-03-21 13:21:32 +0530

    done with training data and made thebag_of_word function in nltk_utils file

--- nltk_utils.py
+++ nltk_utils.py
@@ -1,5 +1,6 @@
 # Importing required libraries
 import nltk
+import numpy as np
 from nltk.stem.porter import PorterStemmer
 
 nltk.download('punkt')
@@ -18,9 +19,21 @@ def stem(word):
 
 # bag of words
 def bag_of_words(tokenized_sentence, all_words):
-    pass
+    """
+    sentence = ["hello", "how", "are", "you"]\
+    words = ["hi", "hello", "I", "you", "bye", "thank", "cool"]
+    bag = [    0,     1,    0,      1,    0,      0,        0]
+    :param tokenized_sentence:
+    :param all_words:
+    :return:
+    """
 
+    tokenized_sentence = [stem(w) for w in tokenized_sentence]
+    bag = np.zeros(len(all_words), dtype=np.float32)
 
-words = ['Organize', 'organizes', 'organizing']
-stemmed_words = [stem(w) for w in words]
-print(stemmed_words)
+    # loop over all_words
+    for idx, w in enumerate(all_words):
+        if w in tokenized_sentence:
+            bag[idx] = 1.0
+
+    return bag
--- train.py
+++ train.py
@@ -1,7 +1,72 @@
 # importing required libraries
 import json
+import numpy as np
+from nltk_utils import tokenize, stem, bag_of_words
+import torch
+import torch.nn
+from torch.utils.data import Dataset, DataLoader
 
+# Open the json file
 with open('intents.json', 'r') as f:
     intents = json.load(f)
 
-print(intents)
+# creating list for words, tags and xy to hold patterns and tags
+all_words = []
+tags = []
+xy = []
+
+# get the tags from json file
+for intent in intents['intents']:
+    tag = intent['tag']
+    tags.append(tag)
+
+    # to get the patterns form the json file
+    for pattern in intent['patterns']:
+        w = tokenize(pattern)
+        all_words.extend(w)
+        xy.append((w, tag))
+
+ignore_words = ['?', '!', '.', ',']
+all_words = [stem(w) for w in all_words if w not in ignore_words]
+all_words = sorted(set(all_words))
+tags = sorted(set(tags))
+
+# Training data
+x_train = []
+y_train = []
+
+# loop over xy array
+for (pattern_sentence, tag) in xy:
+    # x data
+    bag = bag_of_words(pattern_sentence, all_words)
+    x_train.append(bag)
+
+    # y data
+    label = tags.index(tag)
+    y_train.append(label)
+
+# done with training data
+x_train = np.array(x_train)
+x_train = np.array(x_train)
+
+
+# creating new dataset
+class Chatdataset(Dataset):
+    def __init__(self):
+        self.n_samples = len(x_train)
+        self.x_data = x_train
+        self.y_data = y_train
+
+    def __getitem__(self, index):
+        return self.x_data[index], self.y_data[index]
+
+    def __len__(self):
+        return self.n_samples
+
+
+# creating hyperparameter
+batch_size = 8
+
+# Creating chatdataset variable
+dataset = Chatdataset()
+train_loader = DataLoader(dataset=dataset, batch_size=batch_size, shuffle=True, num_workers=2)
